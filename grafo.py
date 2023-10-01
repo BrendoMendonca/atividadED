dcb2ae86e414fd1ca124680685fa63093a64dce0
unknown <brendomendonca20@gmail.com>
2023-10-01 17:32:37 -0300

    códigos de busca bfs

---
+++ grafo.py
@@ -0,0 +1,33 @@
+import queue #biblioteca para trabalhar com filas
+
+class Grafo:
+    def __init__(self, n): #construtor python
+        self.num_vertices = n
+        self.matriz = [[0 for _ in range(n)] for _ in range(n)] #inicialização da matriz nxn zerada
+        self.list = [[]for _ in range(n)] #inicialização da lista de tamanho n vazia
+
+    def print(self):
+        print(self.matriz)
+        print(self.list)
+    
+    def bfs(self, source):
+        dist = [-1 for _ in range(self.num_vertices)] #cria uma lista com valores -1
+        ant = [-1 for _ in range(self.num_vertices)]
+        isVisited = [False for _ in range(self.num_vertices)] #cria uma lista booleana onde todos os valores sao False
+        Q = queue.Queue() #criação do objeto do tipo Queue(fila)
+        Q.put(source)#adiciona elemento(vertice) na fila
+        isVisited[source] = True #informa à lista que o vertice foi visitado
+        dist[source] = 0
+
+        while Q.empty() != True: #verifica se a lista Q não está vazia
+            p = Q.get() #remove o elemento da lista e o armazena na variável p
+            print("Vertice: " + str(p)) #exibe o elemento removido
+        
+            for v in self.list[p]: #loop para iterar com os elementos da lista de adjacencia do vertice p
+                if isVisited[v] == False: #verifica se o vertice adjacente não foi visitado
+                    dist[v] = dist[p] + 1 #calcula a distancia do vertice adjacente ao vertice de origem
+                    ant[v] = p #atribui o valor de p ao antecessor de v na lista de antecessores
+                    Q.put(v) #adiciona v à fila
+                    isVisited[v] = True #v é marcado como visitado
+        
+        return dist, ant
---
+++ main.py
@@ -0,0 +1,29 @@
+from grafo import Grafo
+
+def load_from(fileName): 
+    f = open(fileName, 'r')
+    n = int(f.readline()) #n recebe a primeira linha do arquivo lido que contem a quantidade de vértices
+
+    g = Grafo(n) #instancia da classe Grafo com parâmetro n
+
+    l = 0 #l representa a linha da matriz
+    for line in f:
+        line.strip() #remove espaços em branco em excesso
+        numeros = line.split("\t") #numeros recebe os elementos da linha separados com \t
+        c = 0 #representa a coluna da matriz
+        for i in numeros:
+            if(c == g.num_vertices): #verifica se chegou ao final da linha
+                break
+            g.matriz[l][c] = int(i) #preenche a matriz com os elementos do arquivo
+            if(g.matriz[l][c] > 0):
+                g.list[l].append(c) #se o valor for maior que 0, o vértice é adicionado à lista de adjacentes
+            
+            c += 1 #incremento da coluna
+        l += 1 #incremento da linha
+    return g
+
+gr = load_from("pcv4.txt")
+gr.print()
+dist, ant = gr.bfs(3)
+print(dist)
+print(ant)
